47be44e80951adf05e3ffb5b52f3f1d3291ccb50
franspaco <franspaco@gmail.com>
2017-02-01 08:19:32 -0600

    Added Naturales perfectos

---
+++ Clase/ArregloNNP/ArregloNNP.cpp
@@ -0,0 +1,66 @@
+
+#include <iostream>
+
+class NNP {
+private:
+  int data;
+  bool is_perfect(int inpt){
+    if (inpt < 6)
+      return false;
+
+    int sum = 1;
+    for(int i = 2; i <= (inpt/2); i++){
+      if(inpt%i == 0){
+        sum += i;
+      }
+    }
+    return sum == inpt;
+  }
+public:
+  int& operator=(int const& inpt){
+    if(is_perfect(inpt)){
+      data = inpt;
+      std::cout << "Success!" << std::endl;
+    }else{
+      std::cout << "Error: Not a perfect number!" << std::endl;
+    }
+    return data;
+  }
+  void test(){
+    std::cout << "Test" << std::endl;
+  }
+};
+
+class ArregloNNP {
+private:
+  NNP * array;
+  int size;
+public:
+  ArregloNNP() {}
+  ArregloNNP(int inpt){
+    array = new NNP[inpt];
+    size = inpt;
+  }
+  ~ArregloNNP(){
+    delete[] array;
+  }
+
+  NNP& operator[](int index){
+    if(index < 0  || index >= size){
+      std::cout << "Error: Array out of bounds!" << std::endl;
+      return array[0];
+    }else{
+      return array[index];
+    }
+  }
+};
+
+int main(){
+  ArregloNNP a(10);
+  a[0] = 1;
+  a[1] = 6;
+  a[2] = 28;
+  a[3] = 25;
+
+  return 0;
+}
